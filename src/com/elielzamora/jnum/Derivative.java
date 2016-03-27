c35ab65c20aae80d35c21e9f5ea299ebd84be249
Eliel Zamora <elielzamora@gmail.com>
2016-03-26 22:22:02 -0400

    Changes Derivative to average of slope slightly above and below

--- src/com/elielzamora/jnum/Derivative.java
+++ src/com/elielzamora/jnum/Derivative.java
@@ -7,11 +7,14 @@ public class Derivative {
 		return (y2-y1)/(x2-x1);
 	}
 	public static double prime(Function f, double x, double dx){
-		return (f.val(x+dx) - f.val(x))/dx;
+		double d1 = (f.val(x+dx)-f.val(x))/dx;
+		double d2 = (f.val(x)-f.val(x-dx))/dx;
+		return (d1+d2)/2;
+		//return (f.val(x+dx) - f.val(x))/dx;
 		//same as
 		//return slope(x, x+dx, f.val(x), f.val(x+dx));
 	}
-	public static double prime(Function f, double x){return prime(f,x,0.000000001);}
+	public static double prime(Function f, double x){return prime(f,x,0.00000001);}
 	public static void main(String[] args) {
 		Function sin = new Function(){
 			public double val(double x){
@@ -25,7 +28,15 @@ public class Derivative {
 				Math.PI/2,
 				Math.PI}){
 			System.out.println(prime(sin, x ));
-		
+		}
+		Function x2 = new Function(){
+			public double val(double x){
+				return Math.pow(x, 2);
+			}
+		};
+		for(double x: new double[]{
+				-2,-1,0,1,2}){
+			System.out.println(prime(x2, x ));
 		}
 	}
 }
